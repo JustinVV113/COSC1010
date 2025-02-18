a9fb60c08126840a4923280dedc767e110a1f88f
Justin Velasco <JustinVelasco@westernwyoming.edu>
2025-02-18 07:30:50 +0000

    Ready for grading.

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,14 +1,16 @@
 #
-# Name
-# Date
+# Justin Velasco
+# 2/28/2025
 # Sales Prediction Programming Project
 # COSC 1010
 #
 
+
 # Variables to hold the sales total and the profit
 
 # Get the amount of projected sales.
-
+total_sales = float(input("Enter the projected sales: "))
 # Calculate the projected profit.
-
+profit = total_sales * 0.23
 # Print the projected profit.
+print("The profit is $", format(profit, ',.2f'))
